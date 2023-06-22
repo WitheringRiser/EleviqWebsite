eb1701a97a80f74be676f6e4d5c25efc1c03d03d
WitheringRiser <lukas@brazdeikis.com>
2023-06-22 17:12:31 -0400

    mailchimp complete

---
+++ src/containers/header/CustomForm.js
@@ -0,0 +1,47 @@
+import './header.css';
+
+const url = "https://eleviq.us17.list-manage.com/subscribe/post?u=bce71f9646b44fe898859fbd1&amp;id=103a7871c2&amp;f_id=00b557e0f0";
+
+// simplest form (only email)
+const CustomForm = ({ status, message, className, style, onSubmitted }) => {
+  let input;
+  const submit = () =>
+    input &&
+    input.value.indexOf("@") > -1 &&
+    onSubmitted({
+      EMAIL: input.value
+    });
+
+  return (
+    <>
+    <div className={className} style={style}>
+      <input
+        ref={node => (input = node)}
+        type="email"
+        placeholder="Your email"
+      />
+      <button onClick={submit}>Submit</button>
+    </div>
+    <div className = "eleviq__header-content__input-feedback">
+      {status === "sending" && <div style={{ color: "blue" }}>Sending...</div>}
+      {status === "error" && (
+        <div
+          style={{ color: "red" }}
+          dangerouslySetInnerHTML={{ __html: message }}
+        />
+      )}
+      {status === "success" && (
+        <div
+          style={{ color: "green" }}
+          dangerouslySetInnerHTML={{ __html: message }}
+        />
+      )}
+    </div>
+    </>
+    
+  );
+};
+
+
+
+export default CustomForm;
--- src/containers/header/header.jsx
+++ src/containers/header/header.jsx
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './header.css';
 import ai from '../../assets/person-learning-ai.png';
+import CustomForm from './CustomForm';
+import MailchimpSubscribe from 'react-mailchimp-subscribe';
+
+const url = "https://eleviq.us17.list-manage.com/subscribe/post?u=bce71f9646b44fe898859fbd1&amp;id=103a7871c2&amp;f_id=00b557e0f0";
 
 const Header = () => {
-	return (
-		<div className = "eleviq__header section__padding" id = "home">
-			<div className = "eleviq__header-content">
-				<h1 className = "text">Join our Mailing List for Exclusive Discounts</h1>
-				<p>Get 70% off by entering your email before we launch August 12th</p>
-				<div className = "eleviq__header-content__input">
-					<input type = "email" placeholder = "Your email"></input>
-					<button type = "button">Enter</button>
-				</div>
-			</div>
-			<div className = "eleviq__header-image">
-				<img src = {ai} alt = "ai"/>
-			</div>
-		</div>
-		)
-}
 
-export default Header
+	const handleFormSubmission = formData => {
+		MailchimpSubscribe(url)
+		// Handle the form submission here
+		console.log(formData); // Replace with your logic
+		};
+
+  	return (
+    <>
+      <div className="eleviq__header section__padding" id="home">
+        <div className="eleviq__header-content">
+          <h1 className="text">Join our Mailing List for Exclusive Discounts</h1>
+          <p>Get 70% off by entering your email before we launch August 12th</p>
+		  <MailchimpSubscribe
+          	url={url}
+          	render={({ subscribe, status, message }) => (
+            	<CustomForm
+					className="eleviq__header-content__input"
+              		status={status}
+              		message={message}
+              		onSubmitted={formData => subscribe(formData)}
+            	/>
+          	)}
+          />
+        </div>
+        <div className="eleviq__header-image">
+          <img src={ai} alt="ai" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
